99a97a051fd3fa72ea8ba68349e36c2bb87cbdf9
Takijl7 <Usuario@PC-JORGEL>
2026-04-04 21:17:56 -0600

    Agregue en main ProductoDecorator

--- Proyecto-Progra/ProductoDecorator.h
+++ Proyecto-Progra/ProductoDecorator.h
@@ -1,7 +1,13 @@
 #pragma once
+#include <memory>
 #include "IProducto.h"
 class ProductoDecorator :
     public IProducto
 {
+protected:
+    std::shared_ptr<IProducto> producto;
+public:
+    ProductoDecorator(std::shared_ptr<IProducto> p);
+    virtual ~ProductoDecorator() = default;
 };
 
